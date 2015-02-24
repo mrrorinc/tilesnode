5628386db2543a246251ef64bc310150a4881284
mrrorinc <reflect@mrror.com>
2015-02-24 14:02:58 -0500

    updating readme

--- module/server.js
+++ module/server.js
@@ -2,12 +2,18 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var session = require('client-sessions');
 
+var RUN_LOCAL = true;
+
 function start() {
   var API = express();
 
   API.use(function (req, res, next) {
-    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost');
-    res.setHeader('Access-Control-Allow-Origin', 'http://development.mrrorinc-tiles.divshot.io');
+    if (RUN_LOCAL)
+    {
+      res.setHeader('Access-Control-Allow-Origin', 'http://localhost');
+    } else {
+      res.setHeader('Access-Control-Allow-Origin', 'http://development.mrrorinc-tiles.divshot.io');
+    }
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
